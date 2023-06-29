072113f7a110e2f5023073383cffa924b6dcdc86
J_D_Nye <garwoodjdm@gmail.com>
2023-06-29 20:55:15 +1000

    as in title

--- assets/js/SRPscript.js
+++ assets/js/SRPscript.js
@@ -1,7 +1,10 @@
 //inital Marvel API fetch code
 var id = ""
+var characterIndex = 0
+var characterName = ""
+var comics_list = document.getElementById("search-results-container")
 
-entry = "Sp"
+entry = "Spi"
 if (entry!=="") {
     entry = "nameStartsWith=" + entry + "&"
 }
@@ -10,25 +13,30 @@ let idscrapper = "https://gateway.marvel.com/v1/public/characters?" + entry + "t
 fetch (idscrapper)
 .then(function (response) {
     response.json().then(function(data) {
-        id = data.data.results[0].id 
-        console.log(id)
+        console.log(data.data.results[characterIndex].comics.available)
+        while (true) {
+            if (data.data.results[characterIndex].comics.available>0) {
+                id = data.data.results[characterIndex].id
+                console.log(data.data.results[characterIndex].name)
+                characterName = data.data.results[characterIndex].name
+                break;
+            }
+            characterIndex++;
+        }
         let comicscrapper = "https://gateway.marvel.com/v1/public/characters/" + id +"/comics?ts=1&apikey=09c6684b7cdacf3a0b97f764a489708f&hash=011be6f4c78340c4c4da9a1a4a713518"
         fetch (comicscrapper)
         .then(function (response2) {
             response2.json().then(function(data) {
-            console.log(data)
+            for (i=0;i<data.data.results.length;i++) {
+                currentComic = document.createElement("li")
+                currentComic.textContent = data.data.results[i].title
+                comics_list.appendChild(currentComic)
+            }
     })
     })
 })})
 
-/*
-let comicscrapper = "https://gateway.marvel.com/v1/public/characters/" + id +"/comics?ts=1&apikey=09c6684b7cdacf3a0b97f764a489708f&hash=011be6f4c78340c4c4da9a1a4a713518"
-fetch (comicscrapper)
-.then(function (response) {
-    response.json().then(function(data) {
-        console.log(data)
-    })
-})*/
+
 
 // inital Wiki API code
 document.getElementById("form").addEventListener("submit", function(event) {
@@ -59,5 +67,5 @@ document.getElementById("form").addEventListener("submit", function(event) {
         console.log("Error:", error);
       });
   });
-  
+
 
